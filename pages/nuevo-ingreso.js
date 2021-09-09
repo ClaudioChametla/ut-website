ad7a9f9ea46c9fa41ffee8555d3f91e1e35564ba
Claudio Chametla <clauchametla.777@gmail.com>
2021-09-09 16:37:43 -0500

    Creacion del pagina nuevo ingreso

--- pages/nuevo-ingreso.js
+++ pages/nuevo-ingreso.js
@@ -2,20 +2,21 @@ import Head from "next/head";
 import { Flex } from "@chakra-ui/react";
 import Container from "../components/Container";
 import { SubHero } from "../components/Hero";
-import Tabs from "../components/Tabs";
 import BarRoute from "../components/BarRoute/BarRoute";
+import Forms from "../components/Forms/Forms";
 
 export default function Home() {
 	return (
 		<Flex flexDir="column">
 			<Head>
-				<title>UT - Oferta educativa</title>
+				<title>UT - Nuevo Ingreso </title>
 				<meta name="description" content="Universidad Tollancingo" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Container>
-				<SubHero title="NUEVO INGRESO" src="/images/bg_oferta.jpg" />
+				<SubHero title="NUEVO INGRESO" src="/images/nuevoingresobg.jpg" />
 				<BarRoute />
+				<Forms />
 			</Container>
 		</Flex>
 	);
