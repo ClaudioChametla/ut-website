fb2ed9df15040d933c2a7ce1aa5d565904d5d5d3
Claudio Chametla <clauchametla.777@gmail.com>
2021-09-23 09:08:19 -0500

    Pagina de Vinculacion uso de Grid

--- components/Vinculacion/VinculacionMap.js
+++ components/Vinculacion/VinculacionMap.js
@@ -1,27 +1,24 @@
 /* eslint-disable react/no-array-index-key */
-import { Center, Image, Divider } from "@chakra-ui/react";
+import { Center, Image, Divider, Grid, Box } from "@chakra-ui/react";
 import vincualcionLogos from "../../data/vinculacionLogos.json";
 
 const vinculacionMap = () => {
 	const { logosU } = vincualcionLogos;
 	return (
-		<Center m="100" flexDirection={["column", "column", "row", "row"]}>
-			{logosU &&
-				logosU.map((item, index) => (
-					<Center
-						key={`Center-${index}`}
-						as="a"
-						href="/vinculacion"
-						m="5"
-						w={["200%", "150%", "100%", "100%"]}
-						h="auto%"
-					>
-						<Image m="5" src={item.img} alt={item.id} />
-						<Center height={["none", "none", "100px", "200px"]}>
-							<Divider orientation="vertical" />
+		<Center m="100">
+			<Grid templateColumns={["none", "none", "repeat(5, 1fr)", "repeat(5, 1fr)"]}>
+				{logosU &&
+					logosU.map((item, index) => (
+						<Center key={`Center-${index}`} as="a" href="/vinculacion" h="100%">
+							<Box>
+								<Image src={item.img} alt={item.id} />
+							</Box>
+							<Center m="2" height={["none", "none", "50%", "80%"]}>
+								<Divider orientation="vertical" />
+							</Center>
 						</Center>
-					</Center>
-				))}
+					))}
+			</Grid>
 		</Center>
 	);
 };
