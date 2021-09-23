1ca1f64fed61fb2de540be64c9589eeb0fb456b0
Claudio Chametla <clauchametla.777@gmail.com>
2021-09-23 09:13:32 -0500

    Pagina de Vinculacion con uso de Grid y GridItem

--- components/Vinculacion/VinculacionMap.js
+++ components/Vinculacion/VinculacionMap.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import { Center, Image, Divider, Grid, Box } from "@chakra-ui/react";
+import { Center, Image, Divider, Grid, Box, GridItem } from "@chakra-ui/react";
 import vincualcionLogos from "../../data/vinculacionLogos.json";
 
 const vinculacionMap = () => {
@@ -9,14 +9,16 @@ const vinculacionMap = () => {
 			<Grid templateColumns={["none", "none", "repeat(5, 1fr)", "repeat(5, 1fr)"]}>
 				{logosU &&
 					logosU.map((item, index) => (
-						<Center key={`Center-${index}`} as="a" href="/vinculacion" h="100%">
-							<Box>
-								<Image src={item.img} alt={item.id} />
-							</Box>
-							<Center m="2" height={["none", "none", "50%", "80%"]}>
-								<Divider orientation="vertical" />
+						<GridItem key={`GridItem-${index}`}>
+							<Center as="a" href="/vinculacion" h="100%">
+								<Box>
+									<Image src={item.img} alt={item.id} />
+								</Box>
+								<Center m="2" height={["none", "none", "50%", "80%"]}>
+									<Divider orientation="vertical" />
+								</Center>
 							</Center>
-						</Center>
+						</GridItem>
 					))}
 			</Grid>
 		</Center>
